5e5e916ac0f06da5939f74f04c162fbc98c03a60
Zaroukian <1503928007@mil>
2018-09-04 12:52:29 -0400

    Update embedded links to rawgit

--- js_includes/supplements.js
+++ js_includes/supplements.js
@@ -1,24 +1,25 @@
 
 function loadFiles(rootName, condition){
-	var scenarioFile = rootName+"."+condition+".html";
-	var answerFile = rootName+".ans.html";
+	var rawgit = "https://rawgit.com/ezaroukian/ELICIT-2/master/www/";
+	var scenarioFile = rawgit+rootName+"."+condition+".html";
+	var answerFile = rawgit+rootName+".ans.html";
 	
 	console.log("Loading "+scenarioFile+", "+answerFile);
 	
 	$.getScript("clock.js", function( data, textStatus, jqxhr ) {
-		//console.log( data ); // Data returned
 		console.log( "Clock loading: " + textStatus + ", " + jqxhr.status );
+		//console.log( data ); // Data returned
 	});
 
     
 	$("#includedScenario").load(scenarioFile, function( data, textStatus, jqxhr ) {
-		  console.log( data ); // Data returned
 		  console.log( "Scenario loading: " + textStatus + ", " + jqxhr.status );
+		  //console.log( data ); // Data returned
 		}); 
 	
     $("#includedAns").load(answerFile, function( data, textStatus, jqxhr ) {
-		  //console.log( data ); // Data returned
 		  console.log( "Answers loading: " + textStatus + ", " + jqxhr.status );
+		  //console.log( data ); // Data returned
 		}); 
 		
 	console.log("loadFiles() finished");
