879991e033c351ee862d2605475b8809ab9c4a4a
ezaroukian <ezaroukian@gmail.com>
2018-09-20 08:20:09 -0400

    Move latin square to supplement to see if it helps it show up online

--- js_includes/supplements.js
+++ js_includes/supplements.js
@@ -36,3 +36,40 @@ function loadFiles(rootName, condition){
 		
 	console.log("loadFiles() finished");
 }
+
+//from shuffle.js, trying to make work online
+var condName; //Hoping to have this global to reference in html
+// instead of latin square, items = [ [["type","condition"], {...}], ...  ] 
+//function balanceConditions(arrayOfArrays, counter) { 
+function latinSquare(arrayOfArrays, counter) { 
+	var record = { };
+    var idx = counter;
+	//Determine number of conditions
+	var allConds = [];
+	for (var i = 0; i < arrayOfArrays.length; ++i) { //for each item in items 
+		//console.log(arrayOfArrays[i][0].group[0]);
+		allConds.push(arrayOfArrays[i][0].group[0]);
+	}
+	var conds = jQuery.unique(allConds);
+	var numConds = conds.length;//not sure if jQuery will work here 
+	//map condition name/number to a number 
+	for (var i = 0; i < numConds; ++i){ 
+		record[conds[i]] = i; 
+		console.log(conds[i]+": "+i+", "+record[conds[i]]);
+	} 
+	//console.log(record);
+	//Pick the right items for the condition determined by the counter (including items not marked for condition) 
+	var cond = counter % numConds; //Pick 'random' (sequential) condition number for this participant
+	condName = Object.keys(record)[cond];
+	console.log("Condition selected: "+condName+", "+cond);
+    var a = new Array(arrayOfArrays.length);
+	//arrayOfArrays seems like [ [cond 1 item, cond 1 item, ...], [cond 2 item, ...]]
+	//console.log(arrayOfArrays);
+    for (var i = 0; i < arrayOfArrays.length; ++i) { 
+		if(arrayOfArrays[i][0].group[0] == null || record[arrayOfArrays[i][0].group[0]] == cond){ //look up mapped
+			//console.log(arrayOfArrays[i]);//arrayOfArrays[i] looks like an array of items in a given condition
+			a = arrayOfArrays[i]; 
+		}
+	} 
+	return a; //array of arrays for this condition //Do I need a record?
+}
